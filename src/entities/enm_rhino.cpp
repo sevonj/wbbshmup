3a2e155e1ab2c677775c93134a92a80078060fec
sevonj <100710152+sevonj@users.noreply.github.com>
2025-12-16 16:58:10 +0200

    refactor: EnmRhino style cleanup

--- src/entities/enm_rhino.cpp
+++ src/entities/enm_rhino.cpp
@@ -25,26 +25,6 @@ EnmRhino::EnmRhino() {
 
 EnmRhino::~EnmRhino() {}
 
-void EnmRhino::setup_model() {
-	Ref<PackedScene> mdl_res = ResourceLoader::get_singleton()->load(MDL_PATH, "PackedScene");
-	if (mdl_res.is_valid()) {
-		mdl = (Node3D *)mdl_res->instantiate();
-	} else {
-		mdl = Assets::instance_fallback_model();
-	}
-	mdl->set_name("mdl");
-	add_child(mdl);
-}
-
-void EnmRhino::setup_collider() {
-	coll_sphere = (Ref<SphereShape3D>)memnew(SphereShape3D);
-	coll_sphere->set_radius(COLL_R);
-	coll = memnew(CollisionShape3D);
-	coll->set_shape(coll_sphere);
-	coll->set_name("coll");
-	add_child(coll);
-}
-
 void EnmRhino::_ready() {
 	Enemy::_ready();
 
@@ -61,82 +41,100 @@ void EnmRhino::_process(double delta) {
 		DebugDraw::draw_sphere3d(get_global_position(), coll_sphere->get_radius(), COLOR_DEBUG_COLL);
 	}
 
-	if (launched) {
+	if (has_launched) {
 		t_since_launch += delta;
 	}
+
 	if (t_since_launch > LIFETIME) {
 		queue_free();
+		return;
 	}
 
 	Player *player = Game::get_player();
-	if (player && player->get_enabled() && !gave_up) {
-		Vector3 origin = get_global_position();
-		Vector3 player_pos = player->get_global_position();
-		if (origin.distance_to(player_pos) > AI_ALERT_RADIUS) {
-			return;
-		}
-		bool just_launched = !launched;
-		if (!launched) {
-			launched = true;
-			UiHudRhinoAlert *ui = memnew(UiHudRhinoAlert);
-			ui->set_target(this);
-			Game::get_stage()->add_ui(ui);
-			print_line("Rhino alerted");
-		}
-
-		Basis basis = get_global_basis();
-		Vector3 player_vel = player->get_velocity();
-
-		Vector3 los = player_pos - origin;
-		double nav_time = los.length() / SPEED;
-		Vector3 player_rel_intercept_pos = los + (player_vel * nav_time);
-		Vector3 desired_dir = player_rel_intercept_pos.normalized() * AI_NAVGAIN;
-		Basis desired_basis = Basis::looking_at((player_pos + player_rel_intercept_pos + desired_dir) - origin);
-
-		Quaternion rot = Quaternion(basis);
-		Quaternion desired_rot = Quaternion(desired_basis);
-
-		if (just_launched) {
-			// Snap face player
-			set_global_transform(Transform3D(desired_rot, origin));
-			return;
-		}
-
-		double rot_angle = rot.angle_to(desired_rot);
-
-		// Fixes a bug where transform goes bad once every dozen runs or so.
-		// Quaternion::angle_to() likes to return NaN every now and then.
-		if (Math::is_nan(rot_angle)) {
-			rot_angle = 0.0;
-		}
-
-		//print_line("until giveup: ", rot_angle / AI_GIVEUP_ANGLE);
-		if (rot_angle >= AI_GIVEUP_ANGLE) {
-			gave_up = true;
-			print_line("Rhino gave up");
-		}
-
-		Quaternion final_rot = rot.slerp(desired_rot, Math::clamp(rot_angle, 0., AI_TURN_RATE * delta));
-		set_global_transform(Transform3D(final_rot, origin));
-		set_velocity(-get_basis().get_column(2) * SPEED);
+	if (!player || !player->get_enabled() || has_lost_tgt) {
+		move_and_slide();
+		return;
 	}
+
+	Vector3 origin = get_global_position();
+	Vector3 player_pos = player->get_global_position();
+	if (origin.distance_to(player_pos) > AI_ALERT_RADIUS) {
+		return;
+	}
+	bool just_launched = !has_launched;
+	if (!has_launched) {
+		has_launched = true;
+		UiHudRhinoAlert *ui = memnew(UiHudRhinoAlert);
+		ui->set_target(this);
+		Game::get_stage()->add_ui(ui);
+		print_line("Rhino: Alerted");
+	}
+
+	Basis basis = get_global_basis();
+	Vector3 player_vel = player->get_velocity();
+
+	Vector3 los = player_pos - origin;
+	double nav_time = los.length() / SPEED;
+	Vector3 player_rel_intercept_pos = los + (player_vel * nav_time);
+	Vector3 desired_dir = player_rel_intercept_pos.normalized() * AI_NAVGAIN;
+	Basis desired_basis = Basis::looking_at((player_pos + player_rel_intercept_pos + desired_dir) - origin);
+
+	Quaternion rot = Quaternion(basis);
+	Quaternion desired_rot = Quaternion(desired_basis);
+
+	if (just_launched) {
+		// Snap face the player.
+		set_global_transform(Transform3D(desired_rot, origin));
+		return;
+	}
+
+	double rot_angle = rot.angle_to(desired_rot);
+
+	/*
+	 * Fixes a bug where transform goes bad once every dozen runs or so.
+	 * Quaternion::angle_to() likes to return NaN every now and then.
+	 */
+	if (Math::is_nan(rot_angle)) {
+		rot_angle = 0.0;
+	}
+
+	if (rot_angle >= AI_GIVEUP_ANGLE) {
+		has_lost_tgt = true;
+		print_line("Rhino: Lost target");
+	}
+
+	Quaternion final_rot = rot.slerp(desired_rot, Math::clamp(rot_angle, 0., AI_TURN_RATE * delta));
+	set_global_transform(Transform3D(final_rot, origin));
+	set_velocity(-get_basis().get_column(2) * SPEED);
 
 	move_and_slide();
 }
 
 void EnmRhino::die() {
 	emit_signal("died");
-	print_line(get_class(), " died");
+	print_line("Rhino: Died");
 	coll->set_disabled(true);
 	get_tree()->create_timer(HITFLASH_DURATION)->connect("timeout", callable_mp((Node *)this, &EnmRhino::queue_free));
 }
 
-String EnmRhino::get_display_name() {
-	return "Rhino Missile";
+void EnmRhino::setup_model() {
+	Ref<PackedScene> mdl_res = ResourceLoader::get_singleton()->load(MDL_PATH, "PackedScene");
+	if (mdl_res.is_valid()) {
+		mdl = cast_to<Node3D>(mdl_res->instantiate());
+	} else {
+		mdl = Assets::instance_fallback_model();
+	}
+	mdl->set_name("mdl");
+	add_child(mdl);
 }
 
-String EnmRhino::get_editor_model_path() {
-	return "res://assets/characters/enm_boss_powerpark/mdl_powerpark_dummy.obj";
+void EnmRhino::setup_collider() {
+	coll_sphere = (Ref<SphereShape3D>)memnew(SphereShape3D);
+	coll_sphere->set_radius(COLL_R);
+	coll = memnew(CollisionShape3D);
+	coll->set_shape(coll_sphere);
+	coll->set_name("coll");
+	add_child(coll);
 }
 
 } //namespace godot
--- src/entities/enm_rhino.h
+++ src/entities/enm_rhino.h
@@ -12,28 +12,17 @@ namespace godot {
 class EnmRhino : public Enemy {
 	GDCLASS(EnmRhino, Enemy)
 
-private:
-	static constexpr double SPEED = 35.;
 	static constexpr double COLL_R = 1.0;
+	static constexpr const char *MDL_PATH = "res://assets/entities/enm_rhino/mdl_enm_rhino.blend";
+
+	static constexpr double SPEED = 35.0;
+	static constexpr double LIFETIME = 10.0;
 	static constexpr int32_t MAX_HP = 100;
+
 	static constexpr double AI_ALERT_RADIUS = 200.;
 	static constexpr double AI_NAVGAIN = 3.9;
 	static constexpr double AI_TURN_RATE = M_PI * SPEED * 0.01;
 	static constexpr double AI_GIVEUP_ANGLE = M_PI * 0.5;
-	static constexpr double LIFETIME = 10.0;
-
-	static constexpr const char *MDL_PATH = "res://assets/entities/enm_rhino/mdl_enm_rhino.blend";
-
-	Node3D *mdl = nullptr;
-	CollisionShape3D *coll = nullptr;
-	Ref<SphereShape3D> coll_sphere = Ref<SphereShape3D>();
-
-	bool launched = false;
-	bool gave_up = false;
-	double t_since_launch = 0.;
-
-	void setup_model();
-	void setup_collider();
 
 protected:
 	static void _bind_methods();
@@ -47,8 +36,20 @@ public:
 
 	void die() override;
 
-	String get_display_name() override;
-	String get_editor_model_path() override;
+	String get_display_name() override { return "Rhino Missile"; }
+	String get_editor_model_path() override { return "res://assets/characters/enm_boss_powerpark/mdl_powerpark_dummy.obj"; }
+
+private:
+	bool has_launched = false;
+	bool has_lost_tgt = false;
+	double t_since_launch = 0.0;
+
+	Node3D *mdl = nullptr;
+	CollisionShape3D *coll = nullptr;
+	Ref<SphereShape3D> coll_sphere = Ref<SphereShape3D>();
+
+	void setup_model();
+	void setup_collider();
 };
 
 } //namespace godot
